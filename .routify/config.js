3d61301662079233f3964fdf3437c2c6d1d21704
Aidan Tilgner <aidantilgner02@gmail.com>
2021-10-13 22:33:14 -0700

    added routing to template
    Signed-off-by: Aidan Tilgner <aidantilgner02@gmail.com>

---
+++ .routify/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pages: "src/pages",
+  sourceDir: "public",
+  routifyDir: ".routify",
+  ignore: "",
+  dynamicImports: true,
+  singleBuild: false,
+  noHashScroll: false,
+  distDir: "dist",
+  hashScroll: true,
+  extensions: ["html", "svelte", "md", "svx"],
+  started: "2021-10-14T04:56:16.004Z",
+};
